849d7a1813096965997a0bc9031419459f8f63fa
Christian Weyer <christian.weyer@thinktecture.com>
2018-12-25 19:49:22 +0100

    Simple support for JOIN-ing two microservices in the client

--- shopping-client/client/src/modules/app/services/ordersService.ts
+++ shopping-client/client/src/modules/app/services/ordersService.ts
@@ -1,13 +1,25 @@
 import { Injectable } from "@angular/core";
 import { environment } from "../../../environments/environment";
 import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { Observable, from } from "rxjs";
+import { switchMap, mergeMap, toArray } from 'rxjs/operators';
 
 @Injectable()
 export class OrdersService {
   constructor(private _http: HttpClient) {}
 
   public getOrders(): Observable<any[]> {
-    return this._http.get<any>(environment.ordersApiBaseUrl + "orders");
+
+    const ordersList = this._http.get<any>(environment.ordersApiBaseUrl + "orders")
+      .pipe(
+        switchMap(orders => from(orders)),
+        // TODO: loop through all items
+        mergeMap(order => this._http.get<any>(environment.productsApiBaseUrl + "products/" + (<any>order).items[0].id),
+          (order, items) => Object.assign(order, {items: [items]})
+        ),
+        toArray()
+      );
+
+      return ordersList;
   }
 }
