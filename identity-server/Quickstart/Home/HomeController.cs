be889bfd04ce68cb5cfb759e8697cf1338202e3d
Christian Weyer <christian.weyer@thinktecture.com>
2019-04-01 14:00:38 +0200

    Always show the home page

--- identity-server/Quickstart/Home/HomeController.cs
+++ identity-server/Quickstart/Home/HomeController.cs
@@ -28,14 +28,7 @@ namespace IdentityServer
 
         public IActionResult Index()
         {
-            if (_environment.IsDevelopment())
-            {
-                // only show in development
-                return View();
-            }
-
-            _logger.LogInformation("Homepage is disabled in production. Returning 404.");
-            return NotFound();
+            return View();
         }
 
         /// <summary>
